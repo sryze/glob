c6cd07a2d0f0139809ff1ff38da578904486a97a
szx <szx@null.net>
2019-02-05 06:35:06 +0600

    Update API and project structure

--- glob.h
+++ glob.h
@@ -1,5 +1,44 @@
+#ifndef GLOB_H
+#define GLOB_H
+
+#include <string>
 #ifdef _WIN32
-  #include "glob_win32.h"
+  #include <windows.h>
 #else
-  #include "glob_posix.h"
+  #include <dirent.h>
 #endif
+
+namespace glob {
+
+class glob {
+ public:
+  glob(const std::string &pattern);
+  ~glob();
+
+  std::string current_match() const;
+  bool next();
+  bool is_valid() const;
+
+  operator bool() const {
+    return is_valid();
+  }
+
+ private:
+  glob(const glob &) = delete;
+  void operator=(const glob &) = delete;
+
+ private:
+#ifdef _WIN32
+  HANDLE find_handle_;
+  WIN32_FIND_DATA find_data_;
+  bool ok_;
+#else
+  std::string pattern_;
+  DIR *dir_;
+  struct dirent *dir_entry_;
+#endif
+};
+
+} // namespace glob
+
+#endif // !GLOB_H
