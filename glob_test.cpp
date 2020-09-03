98125a022abec5e34821cb7a95888fb1a30f45ee
szx <szx@null.net>
2020-09-03 19:21:48 +0600

    Update .editorconfig and .giitnore

--- glob_test.cpp
+++ glob_test.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char **argv) {
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " pattern1 [pattern2 [...]]"
-                            << std::endl;
+                  << std::endl;
         return 1;
     }
     for (int i = 1; i < argc; i++) {
