42d0851797865bde72fa52a5b9b6fb7679d1168b
Sergey Zolotarev <sryze@protonmail.com>
2022-12-25 03:00:43 +0600

    Print program's base name instead of full path

--- glob_test.cpp
+++ glob_test.cpp
@@ -1,10 +1,21 @@
+#include <cstring>
 #include <iostream>
 #include "glob.h"
 
+static const char * get_base_name(const char* path) {
+    const char *p = path + std::strlen(path) - 1;
+    while (p > path && *p != '\\' && *p != '/') {
+        p--;
+    }
+    return p + 1;
+}
+
 int main(int argc, char **argv) {
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " pattern1 [pattern2 [...]]"
-                  << std::endl;
+        std::cerr
+            << "Usage: " << get_base_name(argv[0]) << ' '
+            << "pattern1 [pattern2 [...]]"
+            << std::endl;
         return 1;
     }
     for (int i = 1; i < argc; i++) {
